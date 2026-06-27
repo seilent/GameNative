3d61ebf65d5c6047e7bc24a6d0a1060c3acd5cbe
seilent <github@seilent.net>
2026-06-28 01:02:10 +0900

    set version to 1.1.0 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,8 +64,8 @@ android {
         buildConfigField("boolean", "XR_BUILD", "false")
         buildConfigField("boolean", "MODERN_XR", "false")
 
-        versionCode = 16
-        versionName = "1.4.0"
+        versionCode = 14
+        versionName = "1.1.0"
 
         buildConfigField("boolean", "GOLD", "false")
         fun secret(name: String) =
