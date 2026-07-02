518455d4c278c67f6bd704c9b9499858c431c28c
seilent <github@seilent.net>
2026-07-02 18:40:04 +0900

    build: vendor javasteam jars in app/libs instead of external checkout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -330,19 +330,9 @@ dependencies {
     implementation("androidx.browser:browser:1.8.0")
 
     // JavaSteam
-    val localBuild = true // Change to 'true' needed when building JavaSteam manually
-    if (localBuild) {
-        implementation(files("../../JavaSteam/build/libs/javasteam-1.8.0.1-20-SNAPSHOT.jar"))
-        implementation(files("../../JavaSteam/javasteam-depotdownloader/build/libs/javasteam-depotdownloader-1.8.0.1-20-SNAPSHOT.jar"))
-        implementation(libs.bundles.javasteam.dev)
-    } else {
-        implementation(libs.javasteam) {
-            isChanging = version?.contains("SNAPSHOT") ?: false
-        }
-        implementation(libs.javasteam.depotdownloader) {
-            isChanging = version?.contains("SNAPSHOT") ?: false
-        }
-    }
+    implementation(files("libs/javasteam-1.8.0.1-20-SNAPSHOT.jar"))
+    implementation(files("libs/javasteam-depotdownloader-1.8.0.1-20-SNAPSHOT.jar"))
+    implementation(libs.bundles.javasteam.dev)
     implementation(libs.spongycastle)
     implementation(libs.okhttp.dnsoverhttps)
 
